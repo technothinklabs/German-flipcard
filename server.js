687750fd3f5685ecef7badc5a6f3e737c6da0774
technothinklabs <technothink021@gmail.com>
2026-05-03 00:42:30 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // Default route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
